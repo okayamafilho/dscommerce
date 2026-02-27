4d9f8cc6873e491d847077946a9d3d39744b7195
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2026-02-27 10:03:04 -0300

    commit inicial do projeto dscomerce

---
+++ src/main/java/br/com/okayamafilho/dscommerce/User.java
@@ -0,0 +1,14 @@
+package br.com.okayamafilho.dscommerce;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+}
