02dab97c6a0ca3d003d759ef31b91bd15577a13e
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2026-02-28 23:06:14 -0300

    atualizacao relacionamento OrderItem, OrderItemPK

--- src/main/java/br/com/okayamafilho/dscommerce/entities/Order.java
+++ src/main/java/br/com/okayamafilho/dscommerce/entities/Order.java
@@ -1,7 +1,10 @@
 package br.com.okayamafilho.dscommerce.entities;
 
 import java.time.Instant;
-import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,7 +13,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name= "tb_order")
@@ -30,6 +35,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payament payament;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
 
@@ -81,6 +89,18 @@ public class Order {
 
     public void setPayament(Payament payament) {
         this.payament = payament;
-    }     
+    }
 
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.geProduct()).toList();
+    }
+
+    public void setItems(Set<OrderItem> items) {
+        this.items = items;
+    }
+    
 }
--- src/main/java/br/com/okayamafilho/dscommerce/entities/User.java
+++ src/main/java/br/com/okayamafilho/dscommerce/entities/User.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.engine.jdbc.env.spi.IdentifierCaseStrategy;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
