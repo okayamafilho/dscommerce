b82d6ca74e20bdd38562177d208b3064a6b8231f
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2026-03-02 14:19:16 -0300

    Criacao da classe Product Controller

--- dscommerce/src/main/java/br/com/okayamafilho/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/br/com/okayamafilho/dscommerce/controllers/ProductController.java
@@ -2,14 +2,26 @@ package br.com.okayamafilho.dscommerce.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.okayamafilho.dscommerce.entities.Product;
+import br.com.okayamafilho.dscommerce.repositories.ProductRepository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
    
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping
     public String teste() {
-        return "Olá Mundo";
+        Optional<Product> result = repository.findById(1L);
+        Product product = result.get();
+        return product.getName();
     }
 }
---
+++ dscommerce/src/main/java/br/com/okayamafilho/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package br.com.okayamafilho.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.okayamafilho.dscommerce.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+    
+
+
+    
+} 
